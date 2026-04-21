92ea34fce2171c9f5cd8e83cc01c6dd6ce8342a5
vboxuser <vboxuser@Gaurav.myguest.virtualbox.org>
2026-04-21 22:46:10 +0530

    trigger pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Gaurav Singh CI/CD Docker Pipeline"
+    return "Updated from Gaurav Singh CI/CD Docker Pipeline"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
